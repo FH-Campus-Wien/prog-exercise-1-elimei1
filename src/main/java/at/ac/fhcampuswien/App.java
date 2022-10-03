92b63e303a3b5d321ca6d59c871965e94e4cb13f
Elias Meisl <elias.meisl@stud.fh-campswien.ac.at>
2022-10-03 16:54:41 +0200

    fifth step

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class App {
 
     //todo Task 1
-    public void sayHelloWorld(){
+    public void sayHelloWorld() {
         System.out.println("Hello World");
     }
 
     //todo Task 2
-    public void helloRobot(){
+    public void helloRobot() {
         System.out.println("0123456789012345678901");
         System.out.println("           __");
         System.out.println("  _(\\    |@@|");
@@ -24,98 +24,96 @@ public class App {
     }
 
     //todo Task 3
-    public void sumOfLiterals(){
-        char ch= 'Z';
+    public void sumOfLiterals() {
+        char ch = 'Z';
         int hexVal = 0xface;
-        int i1=012;
-        long l=80L;
-        float f1=44e-1f;
-        float f2=5.5f;
-        double d1= 8.88e1;
-        double d2= 99.9;
+        int i1 = 012;
+        long l = 80L;
+        float f1 = 44e-1f;
+        float f2 = 5.5f;
+        double d1 = 8.88e1;
+        double d2 = 99.9;
 
-        int a =(int) f1;
-        int b =(int) f2;
-        int c =(int) l;
-        int d =(int) d1;
-        int e =(int) d2;
-        int f=Character.getNumericValue(ch);
-        int sum= a+b+c+d+e+f+hexVal+i1;
+        int a = (int) f1;
+        int b = (int) f2;
+        int c = (int) l;
+        int d = (int) d1;
+        int e = (int) d2;
+        int f = Character.getNumericValue(ch);
+        int sum = a + b + c + d + e + f + hexVal + i1;
         System.out.println(sum);
-
 
 
     }
 
     //todo Task 4
-    public void addTwoNumbers(){
+    public void addTwoNumbers() {
         Scanner scanner = new Scanner(System.in);
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
-        int sum = num1+num2;
+        int sum = num1 + num2;
         System.out.println(sum);
 
     }
 
     //todo Task 5
-    public void swapTwoNumbers(){
+    public void swapTwoNumbers() {
 
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         int y = scanner.nextInt();
-        x=x+y;
-        y=x-y;
-        x=x-y;
+        x = x + y;
+        y = x - y;
+        x = x - y;
 
-        System.out.println("x:"+x+", y:"+y);
+        System.out.println("x:" + x + ", y:" + y);
 
     }
 
     //todo Task 6
-    public void compareTwoNumbers(){
+    public void compareTwoNumbers() {
         Scanner scanner = new Scanner(System.in);
         int n1 = scanner.nextInt();
         int n2 = scanner.nextInt();
-        if (n1>n2) {System.out.println("n1 > n2");}
-        if (n1>n2) {System.out.println("n2 > n1");}
-        if (n1==n2) {System.out.println("n1 == n2");}
-
-    }
-
-    //todo Task 7
-    public void ratingSalesPerson(){
-
-        Scanner scanner = new Scanner(System.in);
-        int revenue = scanner.nextInt();
-
-        if (revenue <0 )
-        switch(revenue) {
-            case 0> revenue >= i:
-                System.out.println("Invalid Revenue");
-                break;
-            case 0<= revenue <20000:
-                System.out.println("Poor Sales Revenue");
-                break;
-            case :
-                System.out.println("Avarage Sales Revenue");
-                break;
-            case :
-                System.out.println("Good Sales Revenue");
-                break;
-            case :
-                System.out.println("Excellent Sales Revenue");
-                break;
-
+        if (n1 > n2) {
+            System.out.println("n1 > n2");
+        }
+        if (n1 > n2) {
+            System.out.println("n2 > n1");
+        }
+        if (n1 == n2) {
+            System.out.println("n1 == n2");
         }
 
     }
 
+    //todo Task 7
+    public void ratingSalesPerson() {
+
+        Scanner scanner = new Scanner(System.in);
+        int revenue = scanner.nextInt();
+        System.out.println("Enter annual Revenue: ");
+
+        if (revenue < 0 || revenue >= 100000) {
+            System.out.println("Invalid Revenue");
+        } else if (revenue >= 0 && 20000 > revenue) {
+            System.out.println("Poor Sales Revenue");
+        } else if (revenue >= 20000 && 50000 > revenue) {
+            System.out.println("Avarage Sales Revenue");
+        } else if (revenue >= 50000 && 80000 > revenue) {
+            System.out.println("Good Sales Revenue");
+        } else if (revenue >= 80000 && 100000 > revenue) {
+            System.out.println("Excellent Sales Revenue");
+        }
+    }
+
+
     //todo Task 8
-    public void getCommissionRate(){
+    public void getCommissionRate() {
         Scanner scanner = new Scanner(System.in);
         int p = scanner.nextInt();
 
-        switch(p) {
+        switch (p) {
             case 1:
                 System.out.println("Your Commission Rate was set to 0.01");
                 break;
@@ -136,41 +134,38 @@ public class App {
     }
 
     //todo Task 9
-    public void leapyear(){
+    public void leapyear() {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
 
 
-        if ((i % 4 != 0) && i % 400 !=0) {
+        if ((i % 4 != 0) && i % 400 != 0) {
             System.out.println("Leapyear");
-        }
-        else if ((i % 100 ==0 ) && i  % 400 !=0) {
+        } else if ((i % 100 == 0) && i % 400 != 0) {
             System.out.println("Not a Leapyear");
-        }
-        else {
+        } else {
             System.out.println(i + " is not a leap year.");
 
-    }
+        }
     }
 
     //todo Task 10
-    public int transposedNumbers(){
+    public int transposedNumbers() {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
 
-            int reverse = 0;
-            do{
-                reverse= reverse*10+(number%10);
-                number /= 10;
-            } while  (number > 0);
-            System.out.println("Reversed:" + reverse);
-            return reverse;
-        }
+        int reverse = 0;
+        do {
+            reverse = reverse * 10 + (number % 10);
+            number /= 10;
+        } while (number > 0);
+        System.out.println("Reversed:" + reverse);
+        return reverse;
+    }
 
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         App exercise1 = new App();
 
         System.out.println("Task 1: Say Hello World");
